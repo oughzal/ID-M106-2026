1caf9457009e88d9c158952471c0ab477f2c25ac
omar OUGHZAL <omar.oughzal@gmail.com>
2026-04-29 15:27:39 +0100

    commit

---
+++ ID101/S03/exercice01.py
@@ -0,0 +1,7 @@
+# exercice 1
+def adulte(a):
+    if a>=18:
+        return "Adulte"
+    else :
+        return "Mineur"
+    return "Adulte" if a>=18 else "Mineur"
---
+++ ID101/S03/exercice04.py
@@ -0,0 +1,6 @@
+# exercice4
+def surface(b,h):
+    return b*h/2
+
+
+print(surface(10,20))
