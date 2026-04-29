c494b6afcd2961ebdb925efc1e1bb1ad30941ff2
omar OUGHZAL <omar.oughzal@gmail.com>
2026-04-29 17:57:59 +0100

    commit

---
+++ ID101/S03/exercice07.py
@@ -0,0 +1,7 @@
+# exercice 7
+def multiplication(a,b):
+    s=0
+    for i in range(b):
+        s += a
+    return s
+print(multiplication(9,7))
---
+++ ID101/S03/exercice8.py
@@ -0,0 +1,4 @@
+# exercice 8
+def estPair(n):
+    return n%2==0
+print(estPair(9))
